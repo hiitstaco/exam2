f01ce65ec23a71f4399d9e4b1628cf924ce4003f
HiItsTaco <aamirahalladeen@yahoo.com>
2024-12-12 16:28:02 +0000

    rating update

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -91,9 +91,9 @@ export default function Page() {
         <div className="rating gap-3 ml-5">
           <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
           <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" />
-          <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+          <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" defaultChecked />
           <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
-          <input type="radio" name="rating-3" className="mask mask-heart bg-teal-400" defaultChecked />
+          <input type="radio" name="rating-3" className="mask mask-heart bg-teal-400" />
         </div>
 
         <div className="divider divider-secondary"></div>
